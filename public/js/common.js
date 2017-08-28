70df3e950b312c5d48f5135ebc31ff0bb1c8515e
foreezh <522292344@qq.com>
2017-08-28 11:56:03 +0800

    初步完成退出功能

--- public/js/common.js
+++ public/js/common.js
@@ -6,3 +6,17 @@
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
+
+	$('#loginout').click(function(){
+		$.ajax({
+			type:'post',
+			url:'/api/logout',
+			dataType:'json',
+			success:function(data){
+				console.log(data)
+				if(data.code==200){
+					location.href='/main/login'
+				}
+			}
+		})
+	})
